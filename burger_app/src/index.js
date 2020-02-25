8d425b7885b840502591ce43a01271ae7dd26a89
AtharvaThorve <aaathorve@gmail.com>
2020-02-25 17:08:05 +0530

    Basic redux setup completed

--- burger_app/src/index.js
+++ burger_app/src/index.js
@@ -4,11 +4,18 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import reducer from './store/reducer';
+
+const store = createStore(reducer);
 
 const app = (
-	<BrowserRouter>
-		<App />
-	</BrowserRouter>
+	<Provider store={store}>
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
+	</Provider>
 );
 
 ReactDOM.render(app, document.getElementById('root'));
