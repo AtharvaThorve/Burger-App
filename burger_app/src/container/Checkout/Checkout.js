2bc568b1416dc54cfa841d87b8a258adac2d0148
AtharvaThorve <aaathorve@gmail.com>
2020-03-08 11:53:19 +0530

    order fetching from server into redux added

--- burger_app/src/container/Checkout/Checkout.js
+++ burger_app/src/container/Checkout/Checkout.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react';
-import { Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
+import { Redirect, Route } from 'react-router-dom';
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
 import ContactData from './ContactData/ContactData';
-import * as actions from '../../store/actions/index';
 
 class Checkout extends Component {
 	checkoutCancelledHandler = () => {
